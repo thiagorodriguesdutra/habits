5fa93f671e7599db0cfa2759af46345fc6eb905f
Thiago Dutra <thiagorodriguesdutra@users.noreply.github.com>
2023-01-24 21:03:53 -0300

    Working js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,38 @@
-const form = document.getElementById('habits-form');
-const array = [1, 2, 3, 4, 5, 6]
+const form = document.querySelectorAll('form').item(0);
+const today = new Date().toLocaleDateString('pt-Br', {
+    day: 'numeric',
+    month: 'numeric'
+});
+const recordedDays = []
 
-function registerHabitsOfDay() {
-    const fragment = document.createDocumentFragment();
-    array.forEach(item => {
-        const container = fragment
-            .appendChild(document.createElement('div'))
-            .appendChild(document.createElement('div'))
-            .appendChild(document.createElement('input'))
+function handleRecordMyHabits() {
 
-            form.appendChild(fragment)
-    })
-}
+    recordedDays.filter(day => {
+        if(day === today) {
+            alert('Opa, você já registrou este dia!')
+        };
+    });
 
-const button = document.getElementById('registerDay');
-button.addEventListener('click', registerHabitsOfDay);
+    const habitsQuantity = [1, 2, 3, 4, 5, 6];
+    const container = form.appendChild(document.createElement('div'));
+
+    container.classList.add('check-habits');
+
+    habitsQuantity.forEach(habit => {
+        container
+        .appendChild(document.createElement('div'))
+        .appendChild(document.createElement('input')).setAttribute('type', 'checkbox');
+        container.lastElementChild.classList.add('checkmark');
+    });
+    
+    container.insertAdjacentHTML('beforeend', `<div class="date">${today}</div>`);
+
+    recordedDays.push(today);
+
+    callback()
+};
+
+console.log(recordedDays)
+
+const button = document.getElementById('record-button');
+button.addEventListener("click", handleRecordMyHabits);
